9eb087d496cbb6cb1d69defe1c71d01329012607
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-02 23:04:14 +0900

    Title: 입력받아 계산, Time: 239ms, Memory: 15MB, Status: Passed - CodeTree

--- 240502/입력받아 계산/input-calculate.js
+++ 240502/입력받아 계산/input-calculate.js
@@ -1,6 +1,5 @@
-const fs = require ("fs");
+const fs = require("fs");
 
-let input;
-input = fs.readFileSync(0).toString();
+let input = Number(fs.readFileSync(0).toString());
 
-console.log(input);
+console.log(input + 2);
