c7e4fad7ab9bb26ec4c30e7b2644c3065b3030be
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-03 09:43:44 +0900

    Title: 날짜 변경하여 출력, Time: 177ms, Memory: 14MB, Status: Passed - CodeTree

--- 240503/날짜 변경하여 출력/print-date-with-different-format.js
+++ 240503/날짜 변경하여 출력/print-date-with-different-format.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString();
-data=input.split(".");
-year=data[0];
-month=data[1];
-date=data[2];
+date=input.split(".");
 
-console.log(`${date}-${month}-${year}`);
+yy=date[0];
+mm=date[1];
+dd=date[2];
+
+console.log(`${mm}-${dd}-${yy}`);
