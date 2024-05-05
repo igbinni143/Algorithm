e75f61b71ca8cdc7ec07653d35a613bd93346415
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-05 17:43:01 +0900

    Title: 체질량지수, Time: 178ms, Memory: 14MB, Status: Passed - CodeTree

--- 240505/체질량지수/bmi.js
+++ 240505/체질량지수/bmi.js
@@ -1,9 +1,8 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString();
-
 input1 = input.split(" ");
 h = Number(input1[0]);
-w = Number(input1[2]);
+w = Number(input1[1]);
 bb = w/((h/100)**2);
 b = parseInt(bb);
 
