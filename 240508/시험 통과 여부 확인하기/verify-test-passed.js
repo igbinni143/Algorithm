0e581996105eae12995415ace5c0009ca64a8c4b
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-08 22:49:46 +0900

    Title: 시험 통과 여부 확인하기, Time: 182ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240508/시험 통과 여부 확인하기/verify-test-passed.js
@@ -0,0 +1,12 @@
+const fs = require ("fs");
+let input = fs.readFileSync(0).toString().trim();
+
+let a = Number(input);
+
+if (a>=87) {
+  console.log("pass");
+}
+else {
+  b = 80-a
+  console.log("{b} more score")
+}
