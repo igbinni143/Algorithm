8a0c806a305f4d14c7705d7ca45bcbef3e1d0360
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-08 23:03:58 +0900

    Title: 비교에 따른 연산, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240508/비교에 따른 연산/operation-based-on-comparison.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString();
+
+input1=input.split[""];
+
+a = Number(input[0]);
+b = Number(input[1]);
+
+if (a>b) {
+    console.log(a*b)
+}
+else {
+     ${parseInt(b/a)}
+    console.log()
+}
