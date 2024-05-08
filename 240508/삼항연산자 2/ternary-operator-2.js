6ecd6e1cbad4fdb91f4e3b29be767ec8370e8760
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-08 23:42:02 +0900

    Title: 삼항연산자 2, Time: 175ms, Memory: 14MB, Status: Passed - CodeTree

--- 240508/삼항연산자 2/ternary-operator-2.js
+++ 240508/삼항연산자 2/ternary-operator-2.js
@@ -1,4 +1,4 @@
 const fs = require ("fs")
 let input = fs.readFileSync(0).toString();
 a = Number(input)
-if b = a==1? console.log("t") : console.log("f");
+let b = a==1? console.log("t") : console.log("f");
