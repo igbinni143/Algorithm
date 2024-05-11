18048a372ded3895e2dd495b8cc54c0bee97773b
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-12 00:00:05 +0900

    Title: 두 번의 연산, Time: 168ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240512/두 번의 연산/two-operations.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let input = Number(fs.readFileSync(0).toString().trim());
+a = Number()
+if (input %2 ==1) {
+    a = input +3
+}
+
+if (a%3==0) {
+    a = a/3
+}
+
+console.log(a)
