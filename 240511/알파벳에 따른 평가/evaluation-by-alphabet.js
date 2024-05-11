47043b823c7968b58b5a51a83db24059be23f9d7
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-11 10:43:44 +0900

    Title: 알파벳에 따른 평가, Time: 167ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240511/알파벳에 따른 평가/evaluation-by-alphabet.js
+++ 240511/알파벳에 따른 평가/evaluation-by-alphabet.js
@@ -1,20 +1,20 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim();
 
-if (input >= 'S') {
+if (input == 'S') {
     console.log("Superior")
 }
-else if (input >= 'A') {
+else if (input == 'A') {
     console.log("Excellent")
 }
-else if (input >= 'B') {
+else if (input == 'B') {
     console.log("Good")
 }
-else if (input >= 'C') {
+else if (input == 'C') {
     console.log("Effort")
 
 }
-else if (input >= 'D') {
+else if (input == 'D') {
     console.log("Effort")
 }
 else{
