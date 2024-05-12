21aafc0ecc6b00fe0e91cde05f2ae55338c9a60e
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-13 00:04:53 +0900

    Title: 비교 연산, Time: 251ms, Memory: 14MB, Status: Passed - CodeTree

--- 240513/비교 연산/comparison-operator.js
+++ 240513/비교 연산/comparison-operator.js
@@ -1,49 +1,13 @@
-// 입력 및 변수 선언
 let fs = require("fs");
-let input = fs.readFileSync(0).toString().trim().split(" "); //여기서 아예 split 처리하는것도 tip
+let input = fs.readFileSync(0).toString().trim().split(" "); 
 
 let a = Number(input[0]);
 let b = Number(input[1]);
 
 
-if (a >= b) {
-    console.log("1");
-} 
-else {
-    console.log("0");
-}
-//
-if (a > b) {
-    console.log("1");
-} 
-else {
-    console.log("0");
-}
-//
-if (a <= b) {
-    console.log("1");
-} 
-else {
-    console.log("0");
-}
-//
-if (a < b) {
-    console.log("1");
-} 
-else {
-    console.log("0");
-}
-
-if (a == b) {
-    console.log("1");
-} 
-else {
-    console.log("0");
-}
-
-if (a != b) {
-    console.log("1");
-} 
-else {
-    console.log("0");
-}
+console.log (a >= b ? 1 : 0);
+console.log(a > b ? 1: 0);
+console.log(a <= b ? 1 : 0);
+console.log(a < b ? 1 : 0);
+console.log(a == b ? 1 : 0);
+console.log(a != b ? 1 : 0);
