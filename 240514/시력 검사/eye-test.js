ea62f92190b7a2c5a3b9d00fbece15abffd30483
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-14 23:37:44 +0900

    Title: 시력 검사, Time: 177ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240514/시력 검사/eye-test.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().split(" ");
+
+reye = Number(input[0]);
+leye = Number(input[1]);
+
+if (reye >= 1.0 && leye >= 1.0) {
+    console.log("High");
+}
+
+else if (reye >= 0.5 && leye >= 0.5){
+    console.log("Middle");
+}
+else {
+    console.log("Low")
+}
