f1269a3b738bfce9b33100a466488792aa570223
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-14 23:48:03 +0900

    Title: 특정 조건 세 정수 비교, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240514/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
+++ 240514/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -5,21 +5,20 @@ a = Number(input[0]);
 b = Number(input[1]);
 c = Number(input[2]);
 
-let fr = String
-let sr = String
+let fr ,sr;
 
 if (a <= b && a<=c) {
-    fr = "1"
+    fr = 1;
 }
 else{
-    fr = "0"
+    fr = 0;
 }
 
 if (a=b && a==c) {
-    sr = "1"
+    sr = 1;
 }
 else {
-    sr = "0"
+    sr = 0;
 }
 
 console.log(fr, sr);
