c9ad0a15225faa9feb63ba8bae8d48785ac554ef
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-17 00:11:00 +0900

    Title: 숫자의 계절은, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240517/숫자의 계절은/season-of-num.js
+++ 240517/숫자의 계절은/season-of-num.js
@@ -3,15 +3,15 @@ let input = fs.readFileSync(0).toString().trim();
 
 m = Number(input);
 
-if (m >=3 || m<=5) {
+if (m >=12 || m<=2) {
+    console.log("Winter");
+}
+else if (m <= 5) {
     console.log("Spring");
 }
-else if (m >=6 || m<=8) {
+else if (m =< 8) {
     console.log("Summer");
 }
-else if (m >= 9 || m<= 11) {
+else if (m=<11 ) {
     console.log("Fall");
-}
-else if (m==12 || m <=2 ) {
-    console.log("Winter");
 }
