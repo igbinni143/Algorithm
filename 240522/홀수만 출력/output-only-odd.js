74217cf13580172a3cd24cbfb31f91ff5742816f
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-22 08:25:45 +0900

    Title: 홀수만 출력, Time: 1970ms, Memory: 160MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240522/홀수만 출력/output-only-odd.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().split(" ");
+
+a = Number(input[0]);
+b = Number(input[1]);
+//console.log(a,b);
+let result = "";
+
+
+if (a%2 ==1){
+    for (i= a; i<=b, i+=2;) {
+        result += i + " ";
+    }
+}
+
+else {
+    for (i= a+1; i <= b, i+=2;) {
+        result += i + " ";
+}
+}
+
+console.log(result);
