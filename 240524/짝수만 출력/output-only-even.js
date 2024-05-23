c40166f7ed975f09d2ba28262612de26c4490cf4
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-24 00:02:06 +0900

    Title: 짝수만 출력, Time: 180ms, Memory: 14MB, Status: Passed - CodeTree

--- 240524/짝수만 출력/output-only-even.js
+++ 240524/짝수만 출력/output-only-even.js
@@ -3,12 +3,19 @@ let input = fs.readFileSync(0).toString().split(" ");
 
 a = Number(input[0]);
 b = Number(input[1]);
+
 let i = a;
-let result =""
+let result ="";
 
 
 while (i <= b) {
+    if (i %2 ==0) {
     result += i + " ";
-    i+=1
+    }
+    else if (i %2 ==1) {
+        i +=1
+        result += i + " ";
+    }
+    i+=2
 }
 console.log(result)
