048dc662ba355f8f94c6bfdebc3142b2ee885ca2
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-24 23:05:20 +0900

    Title: 문자 출력하기, Time: 179ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240524/문자 출력하기/print-text.js
+++ 240524/문자 출력하기/print-text.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString();
 let result = "";
 
-for (let i = 1; i<=8; i++){
-    result += String(input)
+for (let i = 1; i<=8; i++) {
+    result += input
 }
 
 console.log(result);
