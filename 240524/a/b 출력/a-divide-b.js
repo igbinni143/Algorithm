238bd947196a22c9de4ede4f060e755cd7e8c76f
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-24 23:44:51 +0900

    Title: a/b 출력, Time: 202ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240524/a/b 출력/a-divide-b.js
+++ 240524/a/b 출력/a-divide-b.js
@@ -1,23 +1,23 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().split(" ");
-
-let c = a/b; 
+a = Number(input[0]);
+b = Number(input[1]);
+c = a/b; 
 let c2 = c.toString().trim();
 let c3 = c2.split(".");
 let c4 = c3[1];
 let clength = c4.length;
 let wlength = 21 - clength;
 
-a = Number(input[0]);
-b = Number(input[1]);
 
 if (clength <21) {
-    for (let i = 0; i<=wlength; i++ ) {
+    for (let i = 1; i<wlength; i++ ) {
     c2 += "0"
+    c2 = c2.trim()
     }
 }   
 else if (clenth >=21){
-    for (let i = 0; i<=21; i++) {
+    for (let i = 1; i<21; i++) {
        c2 += c4[i]
        c2 = c2.trim()
     }
