61cb335a06cf5bd18165b90a53468becc7444ee6
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-27 23:43:19 +0900

    Title: 높은 수에서 낮은 수까지, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240527/높은 수에서 낮은 수까지/from-high-to-low.js
+++ 240527/높은 수에서 낮은 수까지/from-high-to-low.js
@@ -4,7 +4,7 @@ let input = fs.readFileSync(0).tostring().split(" ")
 a = Number(input[0]);
 b = Number(input[1]);
 let result = [];
-if (a >= b){
+if (a > b){
     for (let i=b; i<=a; i++) {
         result += i + " ";
     }
