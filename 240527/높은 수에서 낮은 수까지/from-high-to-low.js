f38d5edbe722a4b66add6f21474e6437bbfb2f3e
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-27 23:46:31 +0900

    Title: 높은 수에서 낮은 수까지, Time: 171ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240527/높은 수에서 낮은 수까지/from-high-to-low.js
+++ 240527/높은 수에서 낮은 수까지/from-high-to-low.js
@@ -4,15 +4,18 @@ let input = fs.readFileSync(0).toString().split(" ");
 a = Number(input[0]);
 b = Number(input[1]);
 let result = [];
-if (a >= b){
-    for (let i=b; i<=a; i++) {
+if (a > b){
+    for (let i=a; i>=b; i--) {
         result += i + " ";
     }
 }
-else {
-    for (let i=a; i<=b; i--){
+else if (a < b){
+    for (let i=b; i>=a; i--){
         result += i + " ";
     }
+}
+else { 
+    result = ""
 }
 
 console.log(result);
