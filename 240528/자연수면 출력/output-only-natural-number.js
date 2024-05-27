87937fb9bacc4686aa27b7d3d2560db10cefa6b0
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-28 00:00:07 +0900

    Title: 자연수면 출력, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240528/자연수면 출력/output-only-natural-number.js
@@ -0,0 +1,17 @@
+const fs = require ("fs");
+
+let input = fs.readFileSync(0).toString().split(" ");
+
+a = input[0];
+b = Number(input[1]);
+let result = []
+if {(typeof a === Number) {
+    for (let i = 1; i<=b; i++){
+    result += a +""
+    }
+}
+    console.log(result);
+}
+else if ( a<=0) {
+    console.log(0);
+}
