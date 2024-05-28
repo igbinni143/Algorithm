b43281dd3cabaa82282c4f65390f1308cef8a716
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-28 23:48:06 +0900

    Title: 자연수면 출력, Time: 170ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240528/자연수면 출력/output-only-natural-number.js
+++ 240528/자연수면 출력/output-only-natural-number.js
@@ -2,15 +2,15 @@ const fs = require("fs");
 
 let input = fs.readFileSync(0).toString().split(" ");
 
-let a = input[0];
+let a = Number(input[0]);
 let b = Number(input[1]);
 let result = [];
 
-if (!isNaN(Number(a))) {
+if (!isNaN(a)) {
     for (let i = 1; i <= b; i++) {
         result+=a;
     }
-} else if (Number(a) <= 0) {
+} else if (a <= 0) {
     result = 0;
 } else {
 }
