73345d4f2902fb2266d018dafb48bc8748a59273
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-28 23:58:36 +0900

    Title: 자연수면 출력, Time: 219ms, Memory: 47MB, Status: Passed - CodeTree

--- 240528/자연수면 출력/output-only-natural-number.js
+++ 240528/자연수면 출력/output-only-natural-number.js
@@ -1,17 +1,17 @@
+// 변수 선언 및 입력
 const fs = require("fs");
-
-let input = fs.readFileSync(0).toString().split(" ");
-
+let input = fs.readFileSync(0).toString().trim().split(" ");
 let a = Number(input[0]);
 let b = Number(input[1]);
-let result = "";
 
+// 출력
+let result = "";
 if (a > 0) {
     for (let i = 0; i < b; i++) {
         result += a;
-} 
-else {
-    result = "0";
+    }
 }
-
+else {
+    result = 0;
+}
 console.log(result);
