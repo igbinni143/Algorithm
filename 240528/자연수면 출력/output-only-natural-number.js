1a609b1116847df14f4db3ea83902d0e8611b7b8
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-28 23:57:57 +0900

    Title: 자연수면 출력, Time: 201ms, Memory: 14MB, Status: Passed - CodeTree

--- 240528/자연수면 출력/output-only-natural-number.js
+++ 240528/자연수면 출력/output-only-natural-number.js
@@ -4,15 +4,12 @@ let input = fs.readFileSync(0).toString().split(" ");
 
 let a = Number(input[0]);
 let b = Number(input[1]);
-let result = [];
+let result = "";
 
-if (!isNaN(a)) {
-    for (let i = 1; i <= b; i++) {
-        result+=a;
-    }
-    console.log(result);
-} else if (a <= 0) {
-    console.log(0);
+if (a > 0) {
+    result = a.toString().repeat(b);
 } else {
-    
+    result = "0";
 }
+
+console.log(result);
