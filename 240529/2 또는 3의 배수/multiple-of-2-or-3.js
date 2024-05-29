5db4ecda2d17189f55c13cbd789963c5f0ab07dd
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-29 21:38:55 +0900

    Title: 2 또는 3의 배수, Time: 204ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240529/2 또는 3의 배수/multiple-of-2-or-3.js
@@ -0,0 +1,17 @@
+const fs = require("fs")
+
+let n = Number(fs.readFileSync(0).toString().trim())
+let result = []
+for (let i=1; i<=n; i++){
+    if (i % 2 == 0) {
+        result += "1 "
+    }
+    else if (i%3==0) {
+        result +="1 "
+    }
+    else {
+        result += "0 "
+    }
+}
+
+console.log(result);
