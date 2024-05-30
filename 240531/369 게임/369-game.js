f4ec12c711b62eeb2ebba0a9a76eee1c938b3738
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-31 01:20:58 +0900

    Title: 369 게임, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240531/369 게임/369-game.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let n = fs.readFileSync(0).toString().trim();
+nn = Number(n)
+
+if (nn%3 ==0 | n =3){
+    console.log ("0");
+}
+else if ( n=6| n=9) {
+    console.log("0");
+}
+
+else {
+    console.log(n);
+}
