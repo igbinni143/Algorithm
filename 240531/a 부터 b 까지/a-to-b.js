32d93f7b0f519833b1a0843ecad66b24cc8b52ea
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-05-31 01:15:11 +0900

    Title: a 부터 b 까지, Time: 189ms, Memory: 14MB, Status: Passed - CodeTree

--- 240531/a 부터 b 까지/a-to-b.js
+++ 240531/a 부터 b 까지/a-to-b.js
@@ -5,19 +5,19 @@ let a = Number(input[0]);
 let b = Number(input[1]);
 let li = [];
 
-li = a + " "
+li = a
 
 for (let i = a; a<=b; i++) {
     if (a % 2 == 1) {
         a = a*2
         if (a <= b) {
-            li = a + " ";
+            li +=  " " + a;
         }
     }
     else if (a % 2 == 0) {
         a = a+3
         if (a <= b) {
-            li = a + " ";
+            li+=" " + a;
         }
     }
 }
