aff752e11c0437fa4395fcfe34894ec09fed4264
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-10-23 23:25:03 +0900

    Title: 숫자의 증감, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241023/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -0,0 +1,14 @@
+way,num=input().split()
+
+num = int(num)
+if way =="A" :
+    for i in range (1,num+1):
+        print(i,end=" ")
+        i+=1
+elif way == "D":
+    i == num
+    for i in range(1,num+1):
+        print(i,end=" ")
+        i-=1
+        if i==0:
+            break
