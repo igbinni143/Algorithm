67a148ba88c1e239eca653006e3cbe54a25d9f8d
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-10-23 23:37:18 +0900

    Title: 높은 수에서 낮은 수까지, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241023/높은 수에서 낮은 수까지/from-high-to-low.py
+++ 241023/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -6,8 +6,11 @@ b = int(b)
 
 if b > a:
     for i in range (b, a-1, -1):
-        print(i, end=" ")
+        print(i, end=" ");
+
+elif a>b:
+    for i in range (a, b-1. -1):
+        print(i, end=" ");
 
 else:
-    for i in range (a, b-1. -1):
-        print(i, end=" ")
+    print("끝")
