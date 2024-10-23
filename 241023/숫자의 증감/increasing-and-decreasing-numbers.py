878259053226a1e933764953b00c57de1b33cd53
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-10-23 23:30:44 +0900

    Title: 숫자의 증감, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 241023/숫자의 증감/increasing-and-decreasing-numbers.py
+++ 241023/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -4,7 +4,6 @@ num = int(num)
 if way =="A" :
     for i in range (1,num + 1):
         print(i,end=" ")
-
-elif way == "D":
-    for i in range(num,0 -1):
+else:
+    for i in range(num,0, -1):
         print(i,end=" ")
