cbd8a6bce32c90c149171c51c2f2c19b671c9712
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2024-10-23 23:36:32 +0900

    Title: 높은 수에서 낮은 수까지, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241023/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -0,0 +1,13 @@
+a,b = input().split()
+a = int(a)
+b = int(b)
+
+
+
+if b > a:
+    for i in range (b, a-1, -1):
+        print(i, end=" ")
+
+else:
+    for i in range (b, a-1. -1):
+        print(i,end=" ")
