8fe6722d354bba23c20167e363d09244d701f83a
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2025-01-24 08:43:00 +0900

    [Bronze I] Title: 전주 듣고 노래 맞히기, Time: 116 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/31562. 전주 듣고 노래 맞히기/전주 듣고 노래 맞히기.py
+++ 백준/Bronze/31562. 전주 듣고 노래 맞히기/전주 듣고 노래 맞히기.py
@@ -1,25 +1,25 @@
-N, M = map(int, input().split())
-song_dict = {}
+N,M = map(int,input().split())
+song_dict={}
 
-for _ in range(N):
-    data = input().split()
-    title = data[1] 
-    melody = ''.join(data[2:])
-    prefix = melody[:3]  
+for i in range (N):
+    codes=""
+    title_length,title,code = input().split(' ',2)
+    code = code.split()
+    codes = ''.join(code) 
+    pre_code=codes[:3]
 
-    # 딕셔너리에 추가
-    if prefix not in song_dict:
-        song_dict[prefix] = [title]
+    if pre_code not in song_dict:
+        song_dict[pre_code] = [title]
     else:
-        song_dict[prefix].append(title)
+        song_dict[pre_code].append(title)
 
-for _ in range(M):
-    test_melody = ''.join(input().split()) 
-    if test_melody in song_dict:
-        matched_titles = song_dict[test_melody]
-        if len(matched_titles) == 1:
-            print(matched_titles[0])  
+for _ in range (M):
+    new_code = ''.join(input().split())
+    if new_code in song_dict:
+        right_titles = song_dict[new_code]
+        if len(right_titles) == 1:
+            print(right_titles[0])
         else:
-            print("?")  
+            print("?")
     else:
-        print("!") 
+        print("!")
