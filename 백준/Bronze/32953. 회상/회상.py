94b338f3fb1f0f53ace41b92306fe3c66130f977
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2025-01-25 23:12:14 +0900

    [Bronze I] Title: 회상, Time: 48 ms, Memory: 34864 KB -BaekjoonHub

--- 백준/Bronze/32953. 회상/회상.py
+++ 백준/Bronze/32953. 회상/회상.py
@@ -1,20 +1,29 @@
-N, M = map(int, input().split())  # N: 수업 수, M: 기준 수업 수
-count_Student = {}  # 학생 ID별 수업 참여 횟수 저장
+N,M = map(int,input().split())
 
-for _ in range(N):
-    T = int(input())  # 현재 수업의 학생 수 (입력 값에 따라 사용)
-    id_Student = input().split()  # 현재 수업에 참여한 학생 ID 리스트
+count=1
+student_list2=[]
 
-    for student_id in id_Student:
-        if student_id not in count_Student:  # 새로운 학생
-            count_Student[student_id] = 1  # 초기화
-        else:  # 이미 등록된 학생
-            count_Student[student_id] += 1  # 수업 횟수 증가
+for i in range (N):
+  T = int(input())
+  student_list= input().split()
+  student_list2.extend(student_list)
 
-# M개 이상의 수업을 들은 학생 수 계산
-result = 0
-for count in count_Student.values():
-    if count >= M:
-        result += 1
+real_studentlist =[]
+studentdict={}
 
-print(result)
+for student in student_list2:
+  if student in studentdict:
+    studentdict[student]+=1
+  
+  else:
+    studentdict[student]=1
+  
+M_student=[]
+for student, count in studentdict.items():
+  if count >= M:
+    M_student.append(student)
+
+if len(M_student) == 0:
+  print(0)
+else:
+  print(len(M_student))
