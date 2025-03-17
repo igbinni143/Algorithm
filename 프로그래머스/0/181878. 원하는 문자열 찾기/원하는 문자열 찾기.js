25db794b82baa5b6a8da7d575d2bc0487d35d1db
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2025-03-17 14:01:23 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.13 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -0,0 +1,7 @@
+function solution(myString, pat) {
+    let myString2 = myString.toLowerCase()
+    let pat2 = pat.toLowerCase()
+    var answer = 0;
+    if( myString2. includes (pat2)) answer = 1;
+    return answer;
+}
