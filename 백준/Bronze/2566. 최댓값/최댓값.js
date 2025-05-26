dd3a61475f38265e8d2c9bec72ea5cb20603920a
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2025-05-26 12:26:48 +0900

    [Bronze III] Title: 최댓값, Time: 88 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.js
+++ 백준/Bronze/2566. 최댓값/최댓값.js
@@ -1,35 +1,20 @@
-const fs = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const arr = require("fs")
+	.readFileSync("/dev/stdin")
+	.toString()
+	.split("\n")
+	.map((v) => v.split(" ").map((v) => Number(v)));
 
-max_list = Array();
-max_infolist = Array();
+let max = -1;
+let x = -1;
+let y = -1;
 for (let i = 0; i < 9; i++) {
-	let row = fs[i].split(" ").map((v) => Number(v));
-	//console.log(row[0]);
-	let max = -1;
-	let max_index = 0;
-
 	for (let j = 0; j < 9; j++) {
-		if (row[j] > max) {
-			max = row[j];
-			max_index = j;
-			//console.log(`max: ${max}`);
+		if (max < arr[i][j]) {
+			max = arr[i][j];
+			x = j + 1;
+			y = i + 1;
 		}
 	}
-	let max_info = [max, max_index];
-	max_list.push(max_info);
 }
-
-//console.log(max_list); //각 줄 마다의 최댓값
-let finalmax = -1;
-let finalindex1 = 0;
-let finalindex2 = 0;
-for (let i = 0; i < max_list.length; i++) {
-	if (finalmax < max_list[i][0]) {
-		finalmax = max_list[i][0];
-		finalindex1 = i;
-		finalindex2 = max_list[i][1];
-	}
-}
-
-console.log(finalmax);
-console.log(finalindex1 + 1, finalindex2 + 1);
+console.log(max);
+console.log(y, x);
