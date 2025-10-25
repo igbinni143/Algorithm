ffccab2886f50e94a344f7ab735bb2640863de53
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2025-10-25 11:32:19 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,4 +1,9 @@
-function solution(my_string, s, e) {
+function solution (my_string, s,e){
     var answer = '';
+    var str = my_string.substring(s,e+1);
+    var newStr = str.split('').reverse().join('');
+    
+    answer = my_string.replace(str,newStr);
+    
     return answer;
 }
