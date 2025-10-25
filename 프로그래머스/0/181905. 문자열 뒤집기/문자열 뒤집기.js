2a3f0c99de50f5ce50a84bac0e551a8b15a35edb
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2025-10-25 11:27:36 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,21 +1,4 @@
 function solution(my_string, s, e) {
     var answer = '';
-    let reverseString = my_string.slice(s,e)
-    let newrString = ''
-    if (s!==e) {
-        let endString = my_string.slice(e+1,my_string.length)
-        let startString = my_string.slice(0,s)
-  
-        for (let i =e; i>=s; --i){
-        newrString += my_string[i]
-        }  
- startString += newrString
-    startString += endString
-    answer = (startString)
-    }
-else {
-   answer = my_string
-}
-   
     return answer;
 }
