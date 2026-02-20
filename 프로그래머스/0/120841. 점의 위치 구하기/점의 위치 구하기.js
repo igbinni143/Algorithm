e2f12f92e92aea6a361b750667d8c9a7b1e26f65
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2026-02-20 12:38:39 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,22 @@
+function solution(dot) {
+    var answer = 0;
+    const [a,b] =dot
+
+    if (a>0) {
+        if(b>0) {
+            answer =1
+        }
+        else{
+            answer=4
+        }
+    }
+    else{
+         if(b>0){
+            answer=2
+            }
+         else{
+             answer=3
+         }
+    }
+    return answer;
+}
